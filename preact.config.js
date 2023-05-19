fa207f90b134de2e03f754b05d9065bde9cfa592
Jonathan Grupp <jona_blubb@web.de>
2023-05-19 22:54:18 +0200

    build[] new build without publicPath

--- preact.config.js
+++ preact.config.js
@@ -1,10 +1,10 @@
 export default (config, env, helpers) => {
-  config.output.publicPath = '/generative/';
+  // config.output.publicPath = '/generative/';
 
  // use the public path in your app as 'process.env.PUBLIC_PATH'
- config.plugins.push(
-   new helpers.webpack.DefinePlugin({
-     'process.env.PUBLIC_PATH': JSON.stringify(config.output.publicPath || '/')
-   })
- );
+//  config.plugins.push(
+//    new helpers.webpack.DefinePlugin({
+//      'process.env.PUBLIC_PATH': JSON.stringify(config.output.publicPath || '/')
+//    })
+//  );
 };
